06debfe16bdc732fec4b6e1229c6f478bf4dce3a
sajith narayanan <76071132+sajithnarayanan@users.noreply.github.com>
2021-05-21 14:08:33 +0530

    add some changes in explore.

--- src/config/color.js
+++ src/config/color.js
@@ -2,7 +2,7 @@ export default {
     "primary":"blue",
     "secondary": "grey",
 
-    "home" : "#EB6346"
+    "home" : "#EB6346",
 
     "button" :"#e00b4b"
 
--- src/screens/explore/mallStack.js
+++ src/screens/explore/mallStack.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View,Text,StyleSheet } from 'react-native';
+import { View,Text,StyleSheet,Image, ScrollView} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import colors from '../../config/color';
 
 
 
 const Stack = createStackNavigator();
 
-const MyntraScreen=()=> <View style={style.one}>
+const MyntraScreen=()=> <View>
+    <ScrollView>
 <Text>Myntra Mall</Text>
-<Text> HOUSE OF BRANDS</Text>
+<Text style={{color: colors.home}}> HOUSE OF BRANDS</Text>
 <Text> Brand Store In Focus</Text>
+<Image
+style={style.two}
+source={require('../../assets/pic1.jpg')}/>
+<Image
+style={style.two}
+source={require('../../assets/pic2.jpg')}/>
+<Image
+style={style.two}
+source={require('../../assets/pic3.jpeg')}/>
 
+</ScrollView>
     </View>
 
 const MyntraStack =() => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Myntra" component={MyntraScreen} />
+      <Stack.Screen name="Myntra Mall" component={MyntraScreen} />
     </Stack.Navigator>
   );
 }
 const style=StyleSheet.create({
     one:{
-        color:"#EB6346",
-        fontSize:30,
-        justifyContent:"center"
+    justifyContent:"center",
+    alignContent:"center",
+    left:40,
+    top:20,
+    },
+    two:{
+        height:200,
+        width:200
     }
 })
 export default MyntraStack;
