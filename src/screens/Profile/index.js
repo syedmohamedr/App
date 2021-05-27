fe9e63fa4165edab827436d227a6c419132d55d6
syed mohamed <syedmohamed.r12@outlook.com>
2021-05-27 16:46:13 +0530

    align profile screen

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -20,96 +20,95 @@ const Separator = () => (
 const Explore = ({navigation}) => {
   return (
     <ScrollView>
-        <View>
+      <View>
+        
             <View style={styles.one}>
-            <Text />
+          
             </View>
-            <View >
-            </View>
-          </View>
-    <SafeAreaView style={{flex: 1}}> 
-      <View style={{flex: 1, padding: 1}}>
-        <View style={Styles.container}>
+        <View style={{height:80,marginBottom:15,backgroundColor:"white"}}>
+          <View style={{width:"50%",left:"43%",height:60,top:25,marginBottom:10}}>
         <Button
-            style={styles.two}
         title="LOGIN/SIGN UP"
-        color="#f194ff"
+        color="#fc0259"
         onPress={() => Alert.alert('You are trying to log in...')}
       />
+      </View>
       <Image
       style={styles.image}
       source={require('../../assets/profile.jpg')}/>
+      </View>
+      <View style={{marginBottom:15}}>
           <TouchableOpacity
             style={Styles.buttonStyle}
             onPress={() =>
               navigation.navigate(
                 'Order',)
             }>     
-            <Image style={{height:30, width:30,top:40,}}
+            <Image style={{height:30, width:30,top:40, marginTop:-40}}
             source={require('../../assets/cart.png')}/>
-            <Text style={styles.text}>Orders</Text>
-            <Text style={styles.text}>Check your order status</Text>
+            <Text style={{left:40,fontWeight:"bold",top:10}}>Orders</Text>
+            <Text style={{left:40,top: 10,color:"black",fontSize:12}}>Check your order status</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={Styles.buttonStyle}
             onPress={() => navigation.navigate('Help')}>
-              <Image style={{height:30, width:30,top:40,}}
+              <Image style={{height:30, width:30,top:45,marginTop:-40}}
             source={require('../../assets/help.png')}/>
-            <Text style={{left:40}}>Help Center</Text>
-            <Text style={styles.text1}> Help regarding your recent purchase</Text>
+            <Text style={{left:40,fontWeight:"bold",top:15,height:20}}>Help Center</Text>
+            <Text style={{left:37,top: 11,color:"black",fontSize:12}}> Help regarding your recent purchase</Text>
           </TouchableOpacity> 
           <TouchableOpacity
             style={Styles.buttonStyle}
             onPress={() => navigation.navigate('Wishlist')}>
-              <Image style={{height:30, width:30,top:30,}}
+              <Image style={{height:30, width:30,top:40,marginTop:-40}}
             source={require('../../assets/heart.png')}/>
-           <Text style={{left:50}}>Wishlist</Text>
-           <Text style={{left:50,fontSize:12,}}>Your most loved styles</Text>
+           <Text style={{left:40,fontWeight:"bold",top:10}}>Wishlist</Text>
+           <Text style={{left:41,fontSize:12,top:10}}>Your most loved styles</Text>
           </TouchableOpacity>
+          </View>
+          <View style={{marginBottom:15}}>
           <TouchableOpacity
             style={Styles.buttonStyle}
             onPress={() => navigation.navigate('Coupons')}>
-             <Image style={{height:40, width:30,top:20,}}
+             <Image style={{height:40, width:30,top:30,marginTop:-40}}
             source={require('../../assets/icon7.png')}/>
-            <Text style={{left:50}}>Scan for Coupons</Text>
+            <Text style={{left:50,fontWeight:"bold"}}>Scan for Coupons</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={Styles.buttonStyle}
             onPress={() => navigation.navigate('Earn')}>
-               <Image style={{height:30, width:30,top:30,}}
+               <Image style={{height:35, width:35,top:20,marginTop:-30}}
             source={require('../../assets/icon3.png')}/>
-            <Text style={{left:50}}>Refer & Earn</Text>
+            <Text style={{left:50,fontWeight:"bold",top:-5,}}>Refer & Earn</Text>
           </TouchableOpacity>
+          </View>
+          <View style={{backgroundColor:"white",padding:20}}>
           <TouchableOpacity
-            style={Styles.buttonStyle}
             onPress={() => navigation.navigate('FAQ')}>
-            
-            <Text style={{left:50}}>FAQs</Text>
+            <Text style={{left:50,color:"gray",marginBottom:10}}>FAQs</Text>
           </TouchableOpacity>
-      
             <TouchableOpacity
-            style={Styles.buttonStyle}
             onPress={() => navigation.navigate('About Us')}>
-              
-            <Text style={{left:50}}>About Us</Text>
+            <Text style={{left:50,color:"gray",marginBottom:10 }}>About Us</Text>
             </TouchableOpacity>
             <TouchableOpacity
-            style={Styles.buttonStyle}
             onPress={() => navigation.navigate('Terms')}>
-              
-            <Text style={{left:50}}>TERMS OF USE</Text>
+            <Text style={{left:50,color:"gray",marginBottom:10}}>TERMS OF USE</Text>
             </TouchableOpacity>
             <TouchableOpacity
-            style={Styles.buttonStyle}
             onPress={() => navigation.navigate('Privacy')}>
-               
-            <Text style={{left:50}}>PRIVACY POLICY</Text>
+            <Text style={{left:50,color:"gray",marginBottom:10,}}>PRIVACY POLICY</Text>
             </TouchableOpacity>
-        </View>
-        <Separator>
-        </Separator>
+            </View>
+
+
+            <View style={{justifyContent:"center",alignContent:"center",alignItems:"center",marginTop:10,height:100,}}>
+              <Text style={{color:"gray",}}>App VERSION 4.2104.1</Text>
+            </View>
+        
+       
       </View>
-    </SafeAreaView>
+  
     </ScrollView>
   );
 };
@@ -119,25 +118,27 @@ const styles=StyleSheet.create({
     top:10,
   },
   text1:{
-    left:37,
-    top: 18,
-    color:"black",
-    marginBottom:20,
+    
   },
   one:{
-    backgroundColor:"grey",
-    height:100,
-    marginBottom:10,
+    backgroundColor:"#154360",
+    height:130,
     width:"100%",
   },
- two:{
-backgroundColor:"#09F62C"
- },
+      two:{
+      backgroundColor:"white",
+        width:"100%",
+        },
   image:{
     height:120,
     width:120,
-    left:-120,
-    top: -80,
+    left:20,
+    top: -130,
+    borderRadius:5,
+    borderWidth:1,
+    borderColor:"#ffeef4",
+    marginBottom:-100
+
   }
 
 })
