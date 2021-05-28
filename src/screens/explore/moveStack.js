18432186dd1370f4914274e8c318cad5a99de03b
syed mohamed <syedmohamed.r12@outlook.com>
2021-05-28 11:20:54 +0530

    edit in myntra move

--- src/screens/explore/moveStack.js
+++ src/screens/explore/moveStack.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { View, Text,Image, Button } from 'react-native';
+import { View, Text,Image, Button,Alert } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const MoveStack = () => {
     return (
-        <View>
+        <View style={{backgroundColor:"white"}}>
             <Image
-            style={{height:50,width:50,left:150,top:50}}
+            style={{height:20,width:20,left:"42%",top:20}}
             source={require('../../assets/myntra.png')}></Image>
-            <Text style={{left:210,top:15}}>Myntra</Text>
-            <Text style={{top:30,fontSize:30,color:"orange",left:150,fontWeight:"bold"}}>MOVE</Text>
-            <Text style={{top:120,left:145}}>Now's the time to</Text>
+            <Text style={{left:"48%",top:5,fontSize:10,fontWeight:"bold",}}>Myntra</Text>
+            <Text style={{top:3,fontSize:35,color:"orange",left:"38%",fontWeight:"bold"}}>MOVE</Text>
+            <Text style={{top:120,left:145,marginTop:50}}>Now's the time to</Text>
             <Text style={{fontWeight:"bold",top:120,left:105,fontSize:20}}>Walk more, Earn more</Text>
             <Image style={{height:120,width:120,top:170,left:10}}
             source={require('../../assets/icons/one.jpg')}></Image>
@@ -18,12 +18,17 @@ const MoveStack = () => {
             source={require('../../assets/icons/two.jpg')}></Image>
               <Image style={{height:120,width:120,top:-70,left:270}}
             source={require('../../assets/icons/three.jpg')}></Image>
-            <Text style={{left:70}}>Walk and Earn Insider Points, Goodies from</Text>
-            <Text style={{left:80,marginBottom:30}}>Brands you love and Exclusive Perks</Text>
-            <Button style={{top:100,marginBottom:20,backgroundColor:"red",color:"black"}}
-            title="JOIN NOW"></Button>
-            <Text style={{left:8,top:10}}>By joining, you agree to the</Text>
-            <Text style={{color:"red",left:185,top:-9}}>Terms & Conditions</Text>
+            <Text style={{left:70,top:-50}}>Walk and Earn Insider Points, Goodies from</Text>
+            <Text style={{left:90,marginBottom:65,top:-50}}>Brands you love and Exclusive Perks</Text>
+            <View style={{width:"95%",left:10,borderRadius:10}}>
+            <Button
+        title="JOIN NOW"
+        color="#ff224a"
+        onPress={() => Alert.alert('You Try To Join')}
+      />
+      </View>
+            <Text style={{left:40,top:10}}>By joining, you agree to the</Text>
+            <Text style={{color:"red",left:220,top:-9}}>Terms & Conditions</Text>
         </View>
     );
 }
