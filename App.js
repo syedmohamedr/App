51bd24d1ff460083d934609fb738ca9dd12735a5
sajith narayanan <76071132+sajithnarayanan@users.noreply.github.com>
2021-06-05 15:52:35 +0530

    some changes

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import BottomTabNavigation from './src/navigation/bottomTabNavigation';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import Icon from 'react-native-vector-icons/Ionicons';  
+import AntDesign from 'react-native-vector-icons/AntDesign';  
 import Home from './src/screens/home';
 import Category from './src/screens/category';
 import StudioStack from './src/screens/studioScreen';
@@ -35,23 +35,23 @@ const App = () => {
           <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={ProfileStack} options={{
             title:"Home",headerLeft: () => (
-                <Icon.Button name="ios-menu" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
+                <AntDesign name="search1" size={40} width={60} height={60} color="black" backgroundColor="red" onPress={() => navigation.openDrawer()}></AntDesign>
             )
             }} />
                     <Drawer.Screen name="Categories" component={CategoryStack} options={{ title:"Shop By Categories",headerLeft: () => (
-                <Icon.Button name="ios-menu" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
+                <Icon.Button name="lens" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
             )}}/>
         <Drawer.Screen name="Orders" component={ModalStack} options={{ title:"Orders",headerLeft: () => (
-                <Icon.Button name="ios-menu" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
+                <Icon.Button name="menu-unfold" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
             )}}/>
         <Drawer.Screen name="CONTACT US" component={ModalStack} options={{headerLeft: () => (
-                <Icon.Button name="ios-menu" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
+                <Icon.Button name="menu-fold" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
             )}} />
         <Drawer.Screen name="FAQs" component={QuestionStack} options={{headerLeft: () => (
-                <Icon.Button name="ios-menu" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
+                <Icon.Button name="search2" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
             )}} />
         <Drawer.Screen name="LEGAL" component={PolicyStack} options={{headerLeft: () => (
-                <Icon.Button name="ios-menu" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
+                <Icon.Button name="youtube" size={40} width={60} height={60} color="black" backgroundColor="#fff" onPress={() => navigation.openDrawer()}></Icon.Button>
             )}}/>
 
       </Drawer.Navigator>
--- src/navigation/homeStack.js
+++ src/navigation/homeStack.js
@@ -14,10 +14,10 @@ const HomeStack =({navigation}) => {
       <Stack.Screen name="Home" component={HomeScreen} options={{headerTitleAlign:"left", headerLeft: () => (
            <View style={{justifyContent:"space-between",flexDirection:"row"}}>
              <AntDesign name="menu-unfold" size={25} color="#4a4f51" style={{left:4,}} onPress={()=>navigation.openDrawer()} />
-             <AntDesign name="search1" size={25} color="#4a4f51" style={{left:230,}} onPress={()=>navigation.openDrawer()} />
-             <AntDesign name="bells" size={25} color="#4a4f51" style={{left:240}} onPress={()=>navigation.openDrawer()} />
-             <AntDesign name="hearto" size={25} color="#4a4f51" style={{left:250,}} onPress={()=>navigation.openDrawer()} />
-             <AntDesign name="shoppingcart" size={25} color="#4a4f51" style={{left:260,}} onPress={()=>navigation.openDrawer()}/>
+             <AntDesign name="search1" size={25} color="#4a4f51" style={{left:230,}}onPress={() => navigation.openDrawer()}/>
+             <AntDesign name="bells" size={25} color="#4a4f51" style={{left:240}} onPress={()=>navigation.openDrawer()}/>
+             <AntDesign name="hearto" size={25} color="#4a4f51" style={{left:250,}}  onPress={()=>navigation.openDrawer()}/>
+             <AntDesign name="shoppingcart" size={25} color="#4a4f51" style={{left:260,}} />
            </View>
          )   }}  />
 
